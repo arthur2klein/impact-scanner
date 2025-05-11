c233e53770bbbe3f302bec12d64e5ce128f629f7
aklein <arthur2kleingm@gmail.com>
2025-05-11 09:13:43 +0200

    refactor(symbol.rs): add symbol extraction function taking a closure instead of only changed lines
    will allow the extraction of other kind of symbols to extract usage

--- src/symbol.rs
+++ src/symbol.rs
@@ -54,6 +54,99 @@ impl Display for Symbol {
     }
 }
 
+fn walk_tree<F>(
+    node: Node,
+    file: &str,
+    source: &str,
+    symbols: &mut Vec<Symbol>,
+    language: &Languages,
+    scope_stack: &mut Vec<String>,
+    condition: &F,
+) where
+    F: Fn(&Node) -> bool,
+{
+    let new_scope = language.get_name_for_node(node, source);
+    if let Some(ref scope_name) = new_scope {
+        scope_stack.push(scope_name.to_string());
+    }
+    let mut processed = false;
+    for kind in SymbolKind::iter() {
+        let expected_field = language.field_name(kind);
+        if language.has_kind(node.kind(), kind) {
+            processed = true;
+            if let Some(name_node) = node.child_by_field_name(expected_field) {
+                let name = name_node
+                    .utf8_text(source.as_bytes())
+                    .unwrap_or("<unknown>")
+                    .to_string();
+                let line = name_node.start_position().row + 1;
+                if condition(&node) {
+                    symbols.push(Symbol {
+                        name,
+                        line,
+                        file: file.to_string(),
+                        kind: SymbolKind::Function,
+                        is_exported: language.is_exported(node, source),
+                        scope: scope_stack.clone(),
+                    });
+                }
+            }
+        }
+    }
+    if !processed {
+        for child in node.children(&mut node.walk()) {
+            walk_tree(
+                child,
+                file,
+                source,
+                symbols,
+                language,
+                scope_stack,
+                condition,
+            );
+        }
+    }
+    if new_scope.is_some() {
+        scope_stack.pop();
+    }
+}
+
+/// Gets symbols from a parsed file contained in a node matching the given closure.
+///
+/// ## Parameters:
+/// * `tree` (`&tree_sitter::Tree`): File parsed with tree_sitter,
+/// * `file` (`&str`): Name of the file,
+/// * `source` (`&str`): Content of the file,
+/// * `language` (`language::Languages`): Language of the current file,
+/// * `condition` (`Fn(&Node) -> bool`): Condition for a symbol to be extracted.
+///
+/// ## Returns:
+/// * (`Result<Vec<Symbol>>`): List of symbol which matching the closure.
+pub fn extract_symbols<F>(
+    tree: &Tree,
+    file: &str,
+    source: &str,
+    language: &Languages,
+    condition: F,
+) -> Result<Vec<Symbol>>
+where
+    F: Fn(&Node) -> bool,
+{
+    let cursor = tree.walk();
+    let mut symbols = Vec::new();
+    let mut scope_stack = language.scope_from_path(file);
+    walk_tree(
+        cursor.node(),
+        file,
+        source,
+        &mut symbols,
+        language,
+        &mut scope_stack,
+        &condition,
+    );
+    Ok(symbols)
+}
+
 /// Gets changed symbols from a parsed file.
 /// Will extracts symbols present at line which changed.
 ///
@@ -74,73 +167,17 @@ pub fn extract_changed_symbols(
     changed_lines: &HashSet<usize>,
     language: &Languages,
 ) -> Result<Vec<Symbol>> {
-    let cursor = tree.walk();
-    let mut symbols = Vec::new();
-    let mut scope_stack = language.scope_from_path(file);
-    walk_tree(
-        cursor.node(),
+    extract_symbols(
+        tree,
         file,
         source,
-        &mut symbols,
-        changed_lines,
         language,
-        &mut scope_stack,
-    );
-    Ok(symbols)
-}
-
-fn walk_tree(
-    node: Node,
-    file: &str,
-    source: &str,
-    symbols: &mut Vec<Symbol>,
-    changed_lines: &HashSet<usize>,
-    language: &Languages,
-    scope_stack: &mut Vec<String>,
-) {
-    let new_scope = language.get_name_for_node(node, source);
-    if let Some(ref scope_name) = new_scope {
-        scope_stack.push(scope_name.to_string());
-    }
-    for kind in SymbolKind::iter() {
-        let expected_field = language.field_name(kind);
-        if language.has_kind(node.kind(), kind) {
-            if let Some(name_node) = node.child_by_field_name(expected_field) {
-                let name = name_node
-                    .utf8_text(source.as_bytes())
-                    .unwrap_or("<unknown>")
-                    .to_string();
-                let line = name_node.start_position().row + 1;
-                if changed_lines.iter().any(|&changed_line| {
-                    let starting_row = node.start_position().row;
-                    let ending_row = node.end_position().row;
-                    (starting_row <= changed_line) && (changed_line <= ending_row)
-                }) {
-                    symbols.push(Symbol {
-                        name,
-                        line,
-                        file: file.to_string(),
-                        kind: SymbolKind::Function,
-                        is_exported: language.is_exported(node, source),
-                        scope: scope_stack.clone(),
-                    });
-                }
-            }
-        }
-    }
-
-    for child in node.children(&mut node.walk()) {
-        walk_tree(
-            child,
-            file,
-            source,
-            symbols,
-            changed_lines,
-            language,
-            scope_stack,
-        );
-    }
-    if new_scope.is_some() {
-        scope_stack.pop();
-    }
+        &(|node: &Node| {
+            changed_lines.iter().any(|&changed_line| {
+                let starting_row = node.start_position().row;
+                let ending_row = node.end_position().row;
+                (starting_row <= changed_line) && (changed_line <= ending_row)
+            })
+        }),
+    )
 }
